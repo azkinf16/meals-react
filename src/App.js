ad7b35a8f9b89ae1e2fa9867701efaa9879560a9
azkinf16 <azkifajri16@gmail.com>
2022-09-29 22:21:41 +0700

    editing app and adding routing on app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from "react";
 import "./App.css";
 import Navigation from "./components/Navigation";
 import Contents from "./components/Contents";
+import Detail from "./components/Detail"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <Contents />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Contents />} />
+          <Route path="/:id" element={<Detail />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
