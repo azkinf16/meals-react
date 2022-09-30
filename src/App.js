2258083142d8d9a73d174d9906530877001700dd
azkinf16 <azkifajri16@gmail.com>
2022-09-30 15:20:47 +0700

    adding route for navigation and category

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React from "react";
 import "./App.css";
 import Navigation from "./components/Navigation";
 import Contents from "./components/Contents";
-import Detail from "./components/Detail"
+import Detail from "./components/Detail";
+import Category from "./components/Category";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigation />}/>
+          <Route path="/Category/:cat" element={<Category />} />
+        </Routes>
+        <Routes>
           <Route path="/" element={<Contents />} />
-          <Route path="/:id" element={<Detail />} />
+          <Route path="/Detail/:id" element={<Detail />} />
         </Routes>
       </BrowserRouter>
     </div>
