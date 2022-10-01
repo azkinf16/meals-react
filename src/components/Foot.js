e412cc31cc57d222b9f23c9fb332cd4127ef8e19
azkinf16 <azkifajri16@gmail.com>
2022-10-01 10:49:48 +0700

    adding footer component

---
+++ src/components/Foot.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Footer } from "antd/lib/layout/layout";
+
+export default function Foot() {
+  return (
+    <div className="footer">
+      <Footer style={{ textAlign: "center" }}>
+        🔥Meals ©2022 Created by Ant UED
+      </Footer>
+    </div>
+  );
+}
