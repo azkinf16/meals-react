5da2899b4794dddc639ee1380a5d1beb6803b51d
azkinf16 <azkifajri16@gmail.com>
2022-10-01 10:52:59 +0700

    fixing and adding footer component at detail page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -7,9 +7,11 @@ import axios from "axios";
 import { Row, Col, Image } from "antd";
 
 import "./Detail.css";
+import Foot from "./Foot";
 
 export default function Detail() {
   const [meal, setMeal] = useState([]);
+  const [ing, setIng] = useState([]);
   let { id } = useParams();
 
   const getData = async () => {
@@ -17,8 +19,22 @@ export default function Detail() {
       const datas = await axios.get(
         `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
       );
-      setMeal(datas.data.meals[0]);
-      console.log(meal);
+
+      let data = datas.data.meals[0];
+      console.log(data);
+
+      setMeal(data);
+
+      let tempIng = [];
+
+      for (let i = 0; i <= 20; i++) {
+        if (data[`strIngredient${i}`]) {
+          tempIng.push([data[`strIngredient${i}`], data[`strMeasure${i}`]]);
+        }
+      }
+
+      setIng(tempIng);
+      console.log(ing);
     } catch (error) {
       console.log(error);
     }
@@ -33,59 +49,57 @@ export default function Detail() {
       <Navigation />
       <Content
         className="site-layout"
-        style={{ padding: "0 350px", marginTop: 110 }}
+        style={{ padding: "0 200px 45px", marginTop: 100 }}
       >
         <div
           className="site-layout-background"
           style={{
             padding: 24,
-            minHeight: 360,
             display: "flex",
             background: "#F5F5F5",
+            borderRadius: 10,
           }}
         >
-          <Row gutter={550}>
-            <Col span={6}>
-              <Image width={500} src={meal.strMealThumb} />
+          <Row>
+            <Col span={12}>
+              <Image
+                style={{ width: "100%", height: "100%", borderRadius: 10 }}
+                src={meal.strMealThumb}
+              />
             </Col>
-            <Col span={6}>
-              <h1 style={{ fontWeight: 800 }}>{meal.strMeal}</h1>
-              <p>Category&nbsp;:&nbsp;{meal.strCategory}</p>
-              <br />
-              <div className="ingredients">
-                <h2>Ingredients&nbsp;:</h2>
-                <ul style={{ marginLeft: "-20px" }}>
-                  <li>
-                    {meal.strMeasure1}
-                    {meal.strIngredient1}
-                  </li>
-                  <li>
-                    {meal.strMeasure2} {meal.strIngredient2}
-                  </li>
-                  <li>
-                    {meal.strMeasure3} {meal.strIngredient3}
-                  </li>
-                  <li>
-                    {meal.strMeasure4} {meal.strIngredient4}
-                  </li>
-                  <li>
-                    {meal.strMeasure5} {meal.strIngredient5}
-                  </li>
-                </ul>
+            <Col span={12} style={{ paddingLeft: 20 }}>
+              <div className="info">
+                <div className="left">
+                  <h3 style={{ fontWeight: 800 }}>{meal.strMeal}</h3>
+                  <p>Category&nbsp;:&nbsp;{meal.strCategory}</p>
+                  <h2>Ingredients&nbsp;:</h2>
+                  <ul style={{ marginLeft: "-20px" }}>
+                    {ing.map((ingredient) => {
+                      return (
+                        <li>
+                          {ingredient[1]} {ingredient[0]}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                  <br />
+                  <i>
+                    <strong>Tags&nbsp;:&nbsp;</strong>
+                    {meal.strTags}
+                  </i>
+                </div>
+                <div className="right">
+                  <h2>
+                    <i>Instruction</i>
+                  </h2>
+                  <p>{meal.strInstructions}</p>
+                </div>
               </div>
-              <br />
-              <h2>{meal.strArea}&nbsp;FOOD!!</h2>
-              <br />
-              <br />
-              <br />
-              <i>
-                <strong>Tags&nbsp;:&nbsp;</strong>
-                {meal.strTags}
-              </i>
             </Col>
           </Row>
         </div>
       </Content>
+      <Foot />
     </>
   );
 }
